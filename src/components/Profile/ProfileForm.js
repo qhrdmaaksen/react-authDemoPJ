ae8cd06d242c0ea9c6af76f66ccff8917ba1ddd2
vitamin777 <rlaalsdn8@gmail.com>
2022-09-16 04:11:52 +0900

    feat : 패스워드 변경 기능 추가
    -ProfileForm.js 에서 newPasswordInputRef 로 사용자 입력 받기 위한 state 정의하여 enteredNewPassword 에 변경될 입력된 비밀번호 받았음
    -useContext(AuthContext)로 CONTEXT 에 접근하여 authCtx 상수에 대입
    fetch 로 비밀번호 업데이트를 위한 api endpoint 에 접근해 idToken 에
    authCtx.token 으로 저장함 password 는 입력된 패스워드로 저장함
    요청 실패 시 예외처리 추가하였음
    -isLoading state 로 패스워드 변경 요청 시 메시지 출력되도록함
    
    -기능 정상 출력 확인

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,67 @@
+import { useRef, useState, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef(); /*새로운 비밀번호 입력을 위한 ref*/
+  const authCtx = useContext(AuthContext); /*token 을 받기위해 context 접근*/
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitHandler = event => {
+    event.preventDefault(); /*브라우저 기본값 요청 방지*/
+    const enteredNewPassword = newPasswordInputRef.current.value; /*입력된 비밀번호 가져오기*/
+
+    setIsLoading(true);
+    /*추출한 비밀번호를 사용해서 요청을 보냄*/
+    fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAbGL8iPtvZhg9a3RDzYIO--_VbQr9sy5s',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false /*false 로하면 응답데이터는 없음*/
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      },
+    ).then(res => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        /*요청 실패*/
+        return res.json().then(data => {
+          console.log('요청 실패시 응답 data', data);
+          let errorMessage = '비밀번호 변경 오류 발생';
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message;
+          }
+          throw new Error(errorMessage)
+        });
+      }
+    }).then(data => {
+      console.log(data)
+    }).catch(error => {
+      alert(error.message)
+    })
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
+        {isLoading && <p>비밀 번호 변경중 ...</p>}
+        {!isLoading &&
         <button>Change Password</button>
+        }
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
