fefa67e16dd1b0898b325120f7b6bdc93b1674df
vitamin777 <rlaalsdn8@gmail.com>
2022-09-15 19:07:57 +0900

    feat : firebase 로그인 요청 추가
    -AuthForm.js 에서 로그인과 가입 로직이 url 만 다르고 나머지는 같기에
    변수 url 을 추가해 조건에 맞게 url 만 변경하여 요청 로직 적용하도록함
    
    -기능 정상 출력 확인

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -23,28 +23,33 @@ const AuthForm = () => {
     const enteredPassword = emailInputRef.current.value;
 
     setIsLoading(true);
+    let url; /*가입과 로그인이 로직이 url 을 제외하고 같기때문에 변수 url 에 조건에따라 url 만 변경되도록함*/
     if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAbGL8iPtvZhg9a3RDzYIO--_VbQr9sy5s';
     } else {
-      /*가입 요청*/
-      /*firebase 에 project setting 에 있는 api key entered*/
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbGL8iPtvZhg9a3RDzYIO--_VbQr9sy5s',
-        {
-          /*firebase auth rest api doc 보면서 작성하면됨*/
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      ).then(res => {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbGL8iPtvZhg9a3RDzYIO--_VbQr9sy5s';
+    }
+    /*가입 요청*/
+    /*firebase 에 project setting 에 있는 api key entered*/
+    fetch(url, {
+      /*firebase auth rest api doc 보면서 작성하면됨*/
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
         setIsLoading(false); /*요청에대한 응답 상관없이 로딩중 false*/
         if (res.ok) {
           /*요청 성공시*/
+          return res.json();
         } else {
           /*요청 실패시*/
           return res.json().then(data => {
@@ -55,11 +60,16 @@ const AuthForm = () => {
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+            throw new Error(errorMessage); /*에러가 발생한다면 프로미스와 외부 프로미스는 거절됨*/
           });
         }
+      })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+        alert(err.message);
       });
-    }
   };
 
   return (
@@ -75,8 +85,10 @@ const AuthForm = () => {
           <input type="password" id="password" ref={passwordInputRef} required />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>} {/*로딩 중이 아닐때 Login or Create Account 표현*/}
-          {isLoading && <p>요청 전송 중...</p>}{/*로딩 중일때 메시지 출력*/}
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}{' '}
+          {/*로딩 중이 아닐때 Login or Create Account 표현*/}
+          {isLoading && <p>요청 전송 중...</p>}
+          {/*로딩 중일때 메시지 출력*/}
           <button type="button" className={classes.toggle} onClick={switchAuthModeHandler}>
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
