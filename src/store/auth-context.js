d2b55f8a4a05c0633eedf6bd36a6a72473a52d1f
vitamin777 <rlaalsdn8@gmail.com>
2022-09-17 00:09:56 +0900

    feat : 로그인 토큰 만료 시간 타이머 확인 기능 추가
    -auth-context.js 에서 retrieveStoredToken Fn 생성 후 그안에 저장된 토큰을 받고 저장된 만료 기간이있는 시간을 받도록함
    -auth-context.js 에서 남은 시간 1분 미만일때 사용자가 로그인하지 않으면 null 리턴하고 토큰과 만료기간을 삭제토록함 (로컬저장소에서)
    -auth-context.js 에서 남은 시간이있어서 유효한 토큰이있으면 저장된 토큰을 리턴하도록함
    -auth-context.js 에서 AuthContextProvider component 에서 token data 상수에 저장된 토큰 검색 함수를 넣어 initialToken 에 저장된 토큰을 초기값으로 설정토록함
    -auth-context.js 에서 로그인핸들러에서 만료시간을 저장소에서 받도록함,
    로그아웃타이머 변수에 로그아웃했을때 clearTimeout 으로 남은 만료 시간을 clear 하도록함
    -auth-context.js 에서 useEffect 로 만료 시간이 지날때마다 확인할수있는 타이머 설정 및 setTimeout 으로 기간을 clear 하도록함
    
    -기능정상작동확인

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
+
+let logoutTimer;
+
 const AuthContext = React.createContext({
   token: '' /*token 저장소 초기값*/,
   isLoggedIn: false /*로그인 됐을때 state(기본적으로 로그인 상태가 아니기때문에 false)*/,
@@ -16,29 +19,67 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+/*저장된 토큰을 받으며 저장된 만료 시간도 받음*/
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token'); /*저장된 토큰 받기*/
+  const storedExpirationDate = localStorage.getItem('expirationTime'); /*저장된 만료시간 받기*/
+  /*만료 남은 시간 받기*/
+  const remainingTime = calculateRemainingTime(storedExpirationDate);
+  /*남은 시간 1분미만 상태에서 사용자가 로그인하지 않으면 null 리턴, 토큰, 만료기간 삭제*/
+  if (remainingTime <= 3600) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null;
+  }
+  /*남은 시간이있어서 유효한 토큰이있다면 저장된 토큰 리턴*/
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
+
 /*인증 관련 상태를 관리하는 컴포넌트*/
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem('token'); /*토큰 상태 초기화*/
+  const tokenData = retrieveStoredToken(); /*저장된 토큰 검색*/
+  /*사용자가 로그인하면 로컬 저장소에서 토큰을 받고 그걸 최초의 토큰으로 사용*/
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
   const [token, setToken] =
     useState(initialToken); /*토큰을 보고 사용자의 로그인 여부를 유추할 state*/
 
   const userIsLoggedIn = !!token; /*!! 은 참 또는 거짓 값을 boolean 으로 바꿔줌*/
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {/*useEffect 에서 logoutHandler ref 하고있기에 무한루프 방지 useCallback*/
     setToken(null); /*logout 하면 token 없앰*/
     localStorage.removeItem('token'); /*로컬 저장소에서 토큰 삭제*/
-  };
+    localStorage.removeItem('expirationTime') /*로컬저장소에서 만료기간 삭제*/
+    /*로그아웃했을때 만료기간 클리어*/
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  },[]);
 
   const loginHandler = (token, expirationTime) => {
     /*만료 시간 설정*/
     setToken(token);
     localStorage.setItem('token', token); /*token 로컬 저장소에 저장(key,value 같아야함)*/
+    localStorage.setItem('expirationTime', expirationTime); /*만료 시간 받기*/
 
     /*remainingTime 남은 시간*/
     const remainingTime = calculateRemainingTime(expirationTime);
     /*만료 시간되면 자동으로 사용자 로그아웃 시키도록할것임*/
-    setTimeout(logoutHandler, remainingTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  /*만료 시간이 지날때마다 확인할수있는 timer & setTimeout 으로 clear*/
+  useEffect(()=>{
+    if (tokenData){
+      console.log(tokenData.duration)
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration)
+    }
+  },[tokenData, logoutHandler])
 
   const contextValue = {
     token: token,
